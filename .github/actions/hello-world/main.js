3ba87f779efdf09f34a276f58d69e7d844253cd9
ychao1983 <ychao_1983@sina.com>
2021-09-02 09:03:31 +0800

    Update main.js

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,11 +1,11 @@
 
 console.log("Hello World")
 
-const core = require("@action/core");
+const core = require("@actions/core");
 
 
 const firstGreeting =core.getInput("first-greeting");
-const secondGreeting =core.getInput("Second-greeting");
+const secondGreeting =core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
 
 console.log(`Hello ${firstGreeting}`)
@@ -13,3 +13,4 @@ console.log(`Hello ${secondGreeting}`)
 if(thirdGreeting){
    console.log(`Hello ${thirdGreeting}`)
 }
+
